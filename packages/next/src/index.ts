79cf4e758329023c760ee6314708306448560a6f
shinwonse <sinwonse@gmail.com>
2025-02-01 17:20:27 +0900

    Refactor Next.js ESLint configuration to use @wonse/eslint-react package

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * @wonse/eslint-next
  * ESLint configuration for Next.js projects with modern best practices.
- * 
+ *
  * Features:
  * - Next.js specific rules and best practices
  * - React and React Hooks rules
@@ -13,15 +13,10 @@
 import { GLOB_SRC } from "@wonse/eslint-common";
 import type { Linter } from "eslint";
 import nextPlugin from "@next/eslint-plugin-next";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import typescript from "@wonse/eslint-ts";
+import { react } from "@wonse/eslint-react";
 
 import { nextRules } from "./rules/next-rules.js";
-import { reactRules } from "./rules/react-rules.js";
-import { reactHooksRules } from "./rules/react-hooks-rules.js";
-import { a11yRules } from "./rules/a11y-rules.js";
 
 type NextSettings = {
   rootDir?: string[];
@@ -45,9 +40,11 @@ export function next({
   settings = {},
 }: Partial<NextOptions> = {}): Array<Linter.Config> {
   const tsConfig = enableTypeScript ? typescript() : [];
+  const reactConfig = react({ strict });
 
   return [
     ...tsConfig,
+    ...reactConfig,
     {
       name: "wonse/next/ignores",
       ignores: [
@@ -62,31 +59,19 @@ export function next({
       files,
       plugins: {
         "@next/next": nextPlugin,
-        react: reactPlugin,
-        "react-hooks": reactHooksPlugin,
-        "jsx-a11y": jsxA11yPlugin,
       },
       settings: {
         next: {
           ...settings,
         },
-        react: {
-          version: "detect",
-        },
       },
       rules: {
         ...nextRules,
-        ...reactRules,
-        ...reactHooksRules,
-        ...a11yRules,
         ...(strict
           ? {
               "@next/next/no-html-link-for-pages": "error",
               "@next/next/no-sync-scripts": "error",
               "@next/next/no-script-component-in-head": "error",
-              "react/jsx-key": ["error", { checkFragmentShorthand: true }],
-              "react/jsx-no-duplicate-props": ["error", { ignoreCase: true }],
-              "react-hooks/exhaustive-deps": "error",
             }
           : {}),
       },
